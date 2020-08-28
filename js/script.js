4578db20863d3af89c24cccff6a2c23be5b6edc5
willyaditya <willyaditya03@gmail.com>
2020-08-28 20:07:19 +0700

    Error handling for API and preloader
    Added proloader, preloader will be showing until the document is fully loaded/not undefined
    
    Added error handling for API. When API breaks or is invalid, the page will show error section
    
    Other adjustments, such as removing unused file and jQuery cdn link correction

--- js/script.js
+++ js/script.js
@@ -3,15 +3,41 @@ let index = 0;
 let indexImage = 0;
 const totalImage = 7;
 
-fetch('https://quotes-api.archv.id/api/quotes/v1/latest')
-    .then(response => response.json())
-    .then(result => {
-        document.getElementById("quote").innerHTML = result.content[0].content;
-        document.getElementById("quote-author").innerHTML = result.content[0].author;
-        document.getElementById("quote-left").style.color = getRandomColor();
-        arrQuotes = JSON.parse(JSON.stringify(result))
-    });
+function onReady(callback) {
+    fetch('https://quotes-api.archv.id/api/quotes/v1/latest')
+        .then((response) => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error('Something went wrong');
+            }
+        })
+        .then((result) => {
+            var intervalId = window.setInterval(function () {
+                if (document.getElementsByTagName('body')[0] !== undefined) {
+                    window.clearInterval(intervalId);
+                    document.getElementById("quote").innerHTML = result.content[0].content;
+                    document.getElementById("quote-author").innerHTML = result.content[0].author;
+                    document.getElementById("quote-left").style.color = getRandomColor();
+                    arrQuotes = JSON.parse(JSON.stringify(result))
+                    callback.call(this);
+                }
+            }, 1000);
+        })
+        .catch((error) => {
+            setVisible('.section-loader', false);
+            setVisible('.section-error', true);
+        });
+}
 
+function setVisible(selector, visible) {
+    document.querySelector(selector).style.display = visible ? 'block' : 'none';
+}
+
+onReady(function () {
+    setVisible('.section-quote', true);
+    setVisible('.section-loader', false);
+});
 
 function nextQuote() {
     index + 1 <= arrQuotes.content.length - 1 ? ++index : index = 0;
